c2856788a0d251dab202d71c21bb50f3908998bd
Tawfik Ahmed <tawfikahmed1x@gmail.com>
2024-10-18 14:38:11 +0300

    Fix CORS origin Error

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,34 @@
 const nextConfig = {
   images: {
     domains: ["utfs.io"],
-    remotePatterns: [
+  },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "lh3.googleusercontent.com",
+    },
+  ],
+  async headers() {
+    return [
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, rsc, next-router-prefetch, next-router-state-tree, next-url",
+          },
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+        ],
       },
-    ],
+    ];
   },
 };
 
